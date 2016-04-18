f36fdcbda204c2c10a74efb6260a30ee4b0b4e13
Şahin Gülci <sahingulci@gmail.com>
2016-04-18 12:14:34 +0300

    build python script and dockerfiles changed

--- Python_Docker_Script.py
+++ Python_Docker_Script.py
@@ -48,23 +48,52 @@ os.system('docker build -t tge36-build-app .')
 # run a temporary container and save its id
 build_container_id = subprocess.check_output('docker create tge36-build-app' , shell=True)   
 
+print(build_container_id)
+
 # copy build files from container to host
 os.system('docker cp %s:/usr/src/app/build ./mono_build_output' % build_container_id)
 
 # remove the temporary container
 os.system('docker rm %s'% build_container_id)
 
+os.system('docker build -t serviceregistery ./mono_build_output/build/ServiceRegistery')
+
+os.system('docker build -t apigateway ./mono_build_output/build/ApiGateway')
+
+os.system('docker build -t customerservice ./mono_build_output/build/CustomerService')
+
+os.system('docker build -t orderservice ./mono_build_output/build/OrderService')
+
+os.system('docker build -t productservice ./mono_build_output/build/ProductService')
+
+os.system('docker build -t authenticateservice ./mono_build_output/build/AuthenticateService')
+
+os.system('docker build -t movieservice ./mono_build_output/build/MovieService')
+#Node service build
+os.system('docker build -t node_service ./node_service')
+#Node frontend build
+os.system('docker build -t node_frontend ./node_frontend')
 
 
 # TODO : run service docker images , when you run docker images docker cmd should take
 # argument that contain service registery and microservices own url and port
-
-
+Service_Registery_Url = "http://217.78.97.197:5000/api/registery/save/"
+Service_Registery_Url_Info = "http://217.78.97.197:5000/api/registery/getserviceinfo/"
+Service_IP = "217.78.97.197"
+os.system('docker run -pd 5000:5000 serviceregistery "mono" "/app/ServiceRegistery.exe" "http://*:5000"')
+os.system('docker run -pd 5001:5001 apigateway "mono" "/app/ApiGateway.exe" "http://*:5001" %s' % Service_Registery_Url_Info)
+os.system('docker run -pd 5002:5002 customerservice "mono" "/app/CustomerService.exe" "http://*:5002"  %s %s' % (Service_Registery_Url,Service_IP +":5002"))
+os.system('docker run -pd 5003:5003 orderservice "mono" "/app/OrderService.exe" "http://*:5003"  %s %s' % (Service_Registery_Url,Service_IP +":5003"))
+os.system('docker run -pd 5004:5004 productservice "mono" "/app/ProductService.exe" "http://*:5004" %s %s' % (Service_Registery_Url,Service_IP +":5004"))
+os.system('docker run -pd 5005:5005 authenticateservice "mono" "/app/AuthenticateService.exe" "http://*:5005" %s %s' % (Service_Registery_Url,Service_IP +":5005"))
+os.system('docker run -pd 5006:5006 movieservice "mono" "/app/MovieService.exe" "http://*:5006" %s %s' % (Service_Registery_Url,Service_IP +":5006"))
+os.system('docker run -pd 3000:3000 node_service')
+os.system('docker run -pd 8080:8080 node_frontend')
 
 
 # 1 dakika ara ile docker contianer instance'lar?n?n cpu'lar?n? kontrol eden ona
 # göre yeni docker instance yaratan k?s?m
-
+new_port = 5020
 
 containers = cli.containers()
 
@@ -81,26 +110,21 @@ while True:
         #stat = Popen('docker stats --no-stream %s' % container.get("Id"),
         #stdout=PIPE)
         direct_output = subprocess.check_output('docker stats --no-stream %s' % container.get("Id"), shell=True)
-        #tmp = stat.stdout.read()
         c = '%'
         #print([pos for pos, char in enumerate(direct_output) if char == c])
         indexes = [pos for pos, char in enumerate(direct_output) if char == c]
         #print(float(direct_output[indexes[2]-5:indexes[2]].strip()))
-        if float(direct_output[indexes[2] - 5:indexes[2]].strip()) > 0.0:
+        if float(direct_output[indexes[2] - 5:indexes[2]].strip()) > 0.01:
             # burada yeni bir docker instance olu?turulacak
             # container = cli.create_container(image='serviceregistery', command='/bin/sleep 30')
-            print(0)
-        #else:
-        #    #burada sonraki containera geçilecek
-        #    print(1)
-    
-    
-        
+            if container.get("Image") == "authenticateservice":
+                os.system('docker run -pd %d:%d authenticateservice "mono" "/app/AuthenticateService.exe" "http://*:%d" %s' % (new_port,new_port,new_port,Service_Registery_Url))
+                new_port = new_port + 1
         #for k, v in stats.items():
         #    if k == "cpu_stats":
         #        print("PUTVAL %s/%s/%s N:%s" %
         #        (container.get('Names')[0].lstrip('/'),'docker-cpu',types[0],v['cpu_usage']['total_usage']))
 
-    time.sleep(60)  # Delay for 1 minute (60 seconds)
+    time.sleep(120)  # Delay for 1 minute (60 seconds)
 
     
